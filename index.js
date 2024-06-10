bd5dc2358915c60137b67852abfe8d9c96bcfd7c
089u97t <maryanthonytochukwu@gmail.com>
2024-06-10 06:29:23 -0700

    '#'

---
+++ index.js
@@ -0,0 +1,17 @@
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+});
+
+$(document).ready(function() {
+    $('.hero-content').fadeIn(1500); // Fade in animation for the hero content
+    $('.work-experience').slideDown(1500); // Slide down animation for the work experience section
+});
+
+window.addEventListener('resize', function() {
+    var marqueeContainer = document.querySelector('.marquee-container');
+    var marquee = document.querySelector('.marquee');
+    marquee.style.width = marqueeContainer.offsetWidth + 'px';
+  });
